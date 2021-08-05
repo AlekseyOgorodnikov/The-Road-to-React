616e8bfc67a5847725dcb3f57c92a8d1bb6e00f9
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2021-08-05 12:02:03 +0400

    list in react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import './App.css';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
+function getTitle(title) {
+  return title;
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Hello {getTitle('React')}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <br />
+      {list.map((item) => {
+        return (
+          <div key={item.objectID}>
+            <h3>
+              <a href={item.url}>{item.title}</a>
+            </h3>
+            <h2>{item.author}</h2>
+            <span>Comments: {item.num_comments}</span>
+            <br />
+            <span>Points: {item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }

e1bfcb07e6b7e9b30eb878d6236d6bb1e2d61e5b
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2021-08-12 20:40:04 +0400

    Inline Handler in JSX

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 import './App.css';
 import { React, useState, useEffect, useRef } from 'react';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const usePersistentState = (key, initialState) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'http://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  const usePersistentState = (key, initialState) => {
-    const [value, setValue] = useState(
-      localStorage.getItem(key) || initialState
-    );
-
-    useEffect(() => {
-      localStorage.setItem(key, value);
-    }, [value, key]);
-
-    return [value, setValue];
-  };
+  const [stories, setStories] = useState(initialStories);
 
   const [searchTerm, setSearchTerm] = usePersistentState('search', 'Re');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
   };
 
   const searchedStories = stories.filter((story) =>
@@ -58,7 +65,7 @@ const App = () => {
       </InputWithLable>
       <br />
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
@@ -96,19 +103,32 @@ const InputWithLable = ({
   );
 };
 
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} {...item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ title, url, author, num_comments, points }) => (
-  <div>
-    <h2>
-      <a href={url}>{title}</a>
-    </h2>
-    <h3>{author}</h3>
-    <span>Comments: {num_comments}</span>
-    <br />
-    <span>Points: {points}</span>
-  </div>
-);
+const Item = ({ item, onRemoveItem }) => {
+  return (
+    <div>
+      <h2>
+        <a href={item.url}>{item.title}</a>
+      </h2>
+      <h3>{item.author}</h3>
+      <span>Comments: {item.num_comments}</span>
+      <br />
+      <span>Points: {item.points}</span>
+      <br />
+      <button
+        type="button"
+        onClick={() => {
+          onRemoveItem(item);
+        }}
+      >
+        Dissmis
+      </button>
+    </div>
+  );
+};
 
 export default App;
